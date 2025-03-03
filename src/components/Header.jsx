e9e00568e42f88afc3ad3964f5fa9b0f533af220
Brian Mulawka <mulawkabrian@gmail.com>
2025-03-03 15:17:27 +0100

    commit test

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import { ReactComponent as Logo } from './logo.svg';
+
+function App() {
+  return (
+    <div>
+      {/* Logo is an actual React component */}
+      <Logo />
+    </div>
+  );
+}
