c2644ef62830aa7ad99527ea2587aeeb7a8a7560
Brian Mulawka <mulawkabrian@gmail.com>
2025-03-03 15:17:51 +0100

    test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,6 @@ import { ReactComponent as Logo } from './logo.svg';
 
 function App() {
   return (
-    <div>
-      {/* Logo is an actual React component */}
-      <Logo />
-    </div>
+
   );
 }
