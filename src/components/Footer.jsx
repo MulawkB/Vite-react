7f5eb5d77dbc9d480e2b7c947a4e85ffcf76ef20
Brian Mulawka <mulawkabrian@gmail.com>
2025-03-03 15:33:27 +0100

    activite 4

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer>
+            <small>
+                
+            </small>
+        </footer>
+    )
+}
+
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
-import { ReactComponent as Logo } from './logo.svg';
-
-function App() {
+function Header () {
   return (
-
+    <header>
+      <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" 
+      alt="Un mexicain avec un chapeau jouant la guitare"
+      />
+      <nav>
+        <ul>
+          <li>
+            <a href="">Acceuil</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
+
+export default Header;
