8312caee995cc2b4b7714f06096eeaf953014361
Brian Mulawka <mulawkabrian@gmail.com>
2025-03-04 09:59:49 +0100

    Dish jsx update

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,22 +1,12 @@
-function Dish() {
-    return (
-    <div>    
+function Dish({image,name,price}) {
+    return (  
       <div>
-        <h3>Tacos à l’unité</h3>
-        <img src="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"></img>
-        <p>3€</p>
+        <img src={image}
+        alt={name}
+        />
+        <h3>{name}</h3>
+        <p>{price}€</p>
       </div>
-      <div>
-        <h3>Enchiladas</h3>
-        <img src="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"></img>
-        <p>12€</p>
-      </div>
-      <div>
-        <h3>Mole poblano</h3>
-        <img src="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"></img>
-        <p>15€</p>
-      </div>
-    </div>
     );
   }
 
