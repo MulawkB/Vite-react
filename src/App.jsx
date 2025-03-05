c674a613f13c6a79c00764386013a84e282062ad
Brian Mulawka <mulawkabrian@gmail.com>
2025-03-05 11:53:06 +0100

    added stock filter

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,23 @@ function App() {
       isnew: true,
       image: "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg",
       name: "Tacos à l’unité",
-      price: 3
+      price: 3,
+      stock: 12
     },
     {
       image: "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg",
       name: "Enchiladas",
-      price: 12
+      price: 12,
+      stock: 0
     },
     {
       image: "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg",
       name: "Mole poblano",
-      price: 15
+      price: 15,
+      stock: 5
     },
   ];
+  const filteredDishes = dishes.filter( dish => dish.stock );
   return (
     <>
       <Header/>
@@ -29,7 +33,7 @@ function App() {
       <main>
         <Container>
           <Row>
-            {dishes.map((dish, index) => {
+            {filteredDishes.map((dish, index) => {
               return (
                 <Col md={4} key={index}>
                   <Dish 
@@ -37,6 +41,7 @@ function App() {
                   image={dish.image}
                   name={dish.name}
                   price={dish.price}
+                  stock={dish.stock}
                   />
                 </Col>
             );
